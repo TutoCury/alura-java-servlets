1ad4edb01b21369aa7d53c442bbdce82c9d98e7a
gustavo <gustavo@wolk.com.br>
2017-07-19 16:45:05 -0300

    trocando implementação do post novaEmpresa para usar jsp e add arquivo

--- src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -3,7 +3,7 @@ package br.com.alura.gerenciador.web;
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,9 +18,11 @@ public class NovaEmpresa extends HttpServlet {
             throws ServletException, IOException {
         String nome = req.getParameter("nome");
         new EmpresaDAO().adiciona(new Empresa(nome));
-        PrintWriter writer = resp.getWriter();
-        writer.println("<html><body>Empresa " + nome
-                + " adicionada com sucesso</body></html>");
+        
+        req.setAttribute("nome", nome);
+        RequestDispatcher dispatcher = req
+                .getRequestDispatcher("/WEB-INF/paginas/novaEmpresa.jsp");
+        dispatcher.forward(req, resp);
     }
 
 }
